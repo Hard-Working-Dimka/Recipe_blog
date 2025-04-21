ec19c271d12d60f6080e3bece263519424328d15
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2025-04-21 21:24:39 +0300

    update models.py

--- recepies/models.py
+++ recepies/models.py
@@ -16,16 +16,55 @@ class Allergy(models.Model):
         return self.name
 
 
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    quantity_of_ingredients = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='users')
+
+
 class Recipe(models.Model):
     title = models.CharField(max_length=100)
     image = models.ImageField()
     description = models.TextField()
-    ingredients = models.TextField()
+    ingredients = models.ManyToManyField(Ingredient, related_name='recipes')
     diet = models.ManyToManyField(Diet, related_name='recipes')
     allergy = models.ManyToManyField(Allergy, related_name='recipes')
     pub_date = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField()  # для url путей
     likes = models.ManyToManyField(User, related_name='liked_recipes')
+    calories = models.IntegerField()
 
     def __str__(self):
         return self.title
+
+
+class TypeOfSubscription(models.Model):
+    period = models.IntegerField()
+
+    def __str__(self):
+        return self.period
+
+
+class FoodIntake(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    type_of_subscription = models.ForeignKey(TypeOfSubscription, on_delete=models.CASCADE)
+    food_intake = models.ManyToManyField(FoodIntake)
+    number_of_people = models.IntegerField()
+    allergy = models.ManyToManyField(Allergy, related_name='orders')
+
+    def __str__(self):
+        return self.profile.user
