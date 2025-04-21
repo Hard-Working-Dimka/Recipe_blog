f85d06ad4625a7c4e885501a255de86d15926218
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2025-04-21 16:31:00 +0300

    add models

--- recepies/models.py
+++ recepies/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Diet(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Allergy(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField()
+    description = models.TextField()
+    ingredients = models.TextField()
+    diet = models.ManyToManyField(Diet, related_name='recipes')
+    allergy = models.ManyToManyField(Allergy, related_name='recipes')
+    pub_date = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField()  # для url путей
+    likes = models.ManyToManyField(User, related_name='liked_recipes')
+
+    def __str__(self):
+        return self.title
