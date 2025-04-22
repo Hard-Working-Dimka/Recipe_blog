2b9178f0bc5bee061927abd7a29fff678d128939
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2025-04-22 22:35:28 +0300

    ass static and tmp

--- recepies/views.py
+++ recepies/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def show_index(request):
+    return render(request, 'index.html')
+
+
+def show_auth(request):
+    return render(request, 'auth.html')
+
+
+def show_card(request):
+    return render(request, 'card.html')
+
+
+def show_lk(request):
+    return render(request, 'lk.html')
+
+
+def show_order(request):
+    return render(request, 'order.html')
+
+
+def show_registration(request):
+    return render(request, 'registration.html')
