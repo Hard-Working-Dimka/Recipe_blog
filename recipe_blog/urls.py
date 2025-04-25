ddc4db7c6695cb96da3707dc2bc849cc7d3863aa
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2025-04-25 15:45:05 +0300

    add passwd reset

--- recipe_blog/urls.py
+++ recipe_blog/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth.views import LogoutView, PasswordResetView, PasswordResetConfirmView
+from django.contrib.auth.views import LogoutView, PasswordResetView, PasswordResetConfirmView, PasswordResetDoneView, \
+    PasswordResetCompleteView
 from django.urls import path
 from django.conf.urls.static import static
 
@@ -13,16 +14,16 @@ urlpatterns = [
                   path('recipe/<slug:slug>/', show_card, name='recipe_detail'),
                   # path('order/', show_order),
                   # path('profile/', show_lk, name='profile'),
-path('profile/', views.ProfileUser.as_view(), name='profile'),
+                  path('profile/', views.ProfileUser.as_view(), name='profile'),
 
                   path('login/', views.LoginUser.as_view(), name='login'),
                   path('logout/', LogoutView.as_view(), name='logout'),
                   path('register/', views.register, name='register'),
 
-                  # TODO: нормально не работает, посмотреть
-                  path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
-                  path('password_reset/done/', PasswordResetView.as_view(), name='password_reset_done'),
-                  path('password-reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(),
+                  path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
+                  path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+                  path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(),
                        name='password_reset_confirm'),
+                  path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
